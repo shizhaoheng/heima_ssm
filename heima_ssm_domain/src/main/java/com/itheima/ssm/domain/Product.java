7611a633205681af4edcdc5bff2f83f336202e7b
shizhaoheng <mi@123456>
2021-02-04 18:29:44 +0800

    update

--- heima_ssm_domain/src/main/java/com/itheima/ssm/domain/Product.java
+++ heima_ssm_domain/src/main/java/com/itheima/ssm/domain/Product.java
@@ -1,5 +1,11 @@
 package com.itheima.ssm.domain;
 
+
+
+
+
+import com.itheima.ssm.utils.DateUtils;
+
 import java.util.Date;
 
 /**
@@ -61,6 +67,9 @@ public class Product {
     }
 
     public String getDepartureTimeStr() {
+        if (departureTime != null) {
+            departureTimeStr = DateUtils.date2String(departureTime, "yyyy-MM-dd HH:mm:ss");
+        }
         return departureTimeStr;
     }
 
@@ -93,6 +102,15 @@ public class Product {
     }
 
     public String getProductStatusStr() {
+        if (productStatus != null) {
+            // 状态 0 关闭 1 开启
+            if (productStatus == 0) {
+                productStatusStr = "关闭";
+            }
+            if (productStatus == 1) {
+                productStatusStr = "开启";
+            }
+        }
         return productStatusStr;
     }
 
