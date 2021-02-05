7317c781d39f4c37b2505b98764a01ac2411016e
shizhaoheng <mi@123456>
2021-02-05 18:32:08 +0800

    订单操作

--- heima_ssm_domain/src/main/java/com/itheima/ssm/domain/Traveller.java
+++ heima_ssm_domain/src/main/java/com/itheima/ssm/domain/Traveller.java
@@ -5,15 +5,17 @@ package com.itheima.ssm.domain;
  * @Date :   2021/2/5 11:24
  * @Description :
  */
+
+//旅客
 public class Traveller {
     private String id;
     private String name;
     private String sex;
     private String phoneNum;
-    private Integer credentialsType;
+    private Integer credentialsType; //证件类型 0身份证 1护照 2军官证
     private String credentialsTypeStr;
     private String credentialsNum;
-    private Integer travellerType;
+    private Integer travellerType; //旅客类型(人群) 0 成人 1 儿童
     private String travellerTypeStr;
 
     public String getId() {
@@ -57,6 +59,16 @@ public class Traveller {
     }
 
     public String getCredentialsTypeStr() {
+        //证件类型 0身份证 1护照 2军官证
+        if (credentialsType != null) {
+            if (credentialsType == 0) {
+                credentialsTypeStr = "身份证";
+            } else if (credentialsType == 1) {
+                credentialsTypeStr = "护照";
+            } else if (credentialsType == 2) {
+                credentialsTypeStr = "军官证";
+            }
+        }
         return credentialsTypeStr;
     }
 
@@ -81,6 +93,14 @@ public class Traveller {
     }
 
     public String getTravellerTypeStr() {
+        ////旅客类型(人群) 0 成人 1 儿童
+        if (travellerType != null) {
+            if (travellerType == 0) {
+                travellerTypeStr = "成人";
+            } else if (travellerType == 1) {
+                travellerTypeStr = "儿童";
+            }
+        }
         return travellerTypeStr;
     }
 
