4ae4eb8203107d1ab733e51ffa0299ecaf6e5999
shizhaoheng <mi@123456>
2021-02-05 18:24:21 +0800

    订单操作

--- heima_ssm_dao/src/main/java/com/itheima/ssm/dao/IOrdersDao.java
+++ heima_ssm_dao/src/main/java/com/itheima/ssm/dao/IOrdersDao.java
@@ -17,16 +17,16 @@ public interface IOrdersDao {
     //查询所有订单
     @Select("select * from orders")
     @Results({
-            @Result(id=true,property = "id",column = "id"),
-            @Result(property = "orderNum",column = "orderNum"),
-            @Result(property = "orderTime",column = "orderTime"),
-            @Result(property = "orderStatus",column = "orderStatus"),
-            @Result(property = "peopleCount",column = "peopleCount"),
-            @Result(property = "peopleCount",column = "peopleCount"),
-            @Result(property = "payType",column = "payType"),
-            @Result(property = "orderDesc",column = "orderDesc"),
+            @Result(id = true, property = "id", column = "id"),
+            @Result(property = "orderNum", column = "orderNum"),
+            @Result(property = "orderTime", column = "orderTime"),
+            @Result(property = "orderStatus", column = "orderStatus"),
+            @Result(property = "peopleCount", column = "peopleCount"),
+            @Result(property = "peopleCount", column = "peopleCount"),
+            @Result(property = "payType", column = "payType"),
+            @Result(property = "orderDesc", column = "orderDesc"),
 //            @Result(property = "product",column = "productId",javaType = Product.class,one = @One(select = "com.itheima.ssm.dao.IProductDao.findById")),
-            @Result(property = "product",column = "productId",javaType = Product.class,one = @One(select = "com.itheima.ssm.dao.IProductDao.findById")),
+            @Result(property = "product", column = "productId", javaType = Product.class, one = @One(select = "com.itheima.ssm.dao.IProductDao.findById")),
     })
     public List<Orders> findAll() throws Exception;
 
@@ -43,8 +43,8 @@ public interface IOrdersDao {
             @Result(property = "payType", column = "payType"),
             @Result(property = "orderDesc", column = "orderDesc"),
             @Result(property = "product", column = "productId", javaType = Product.class, one = @One(select = "com.itheima.ssm.dao.IProductDao.findById")),
-            @Result(property = "member",column = "memberId",javaType = Member.class,one = @One(select = "com.itheima.ssm.dao.IMemberDao.findById")),
-            @Result(property = "travellers",column = "id",javaType =java.util.List.class,many = @Many(select = "com.itheima.ssm.dao.ITravellerDao.findByOrdersId"))
+            @Result(property = "member", column = "memberId", javaType = Member.class, one = @One(select = "com.itheima.ssm.dao.IMemberDao.findById")),
+            @Result(property = "travellers", column = "id", javaType = java.util.List.class, many = @Many(select = "com.itheima.ssm.dao.ITravellerDao.findByOrdersId"))
     })
     public Orders findById(String ordersId) throws Exception;
 }
